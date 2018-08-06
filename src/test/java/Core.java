b80ce45779d728ac44c2c487e3d776a98e9e15c2
Romans Dobrijans <rdobrijans@evolutiongaming.com>
2018-08-06 18:46:11 +0300

    Finalised version on 06.08.2018

--- src/test/java/Core.java
+++ src/test/java/Core.java
@@ -13,10 +13,8 @@ public class Core {
     private WebDriver driver;
     private static final String CHROME_DRIVER_LOCATION = "C://driver/chromedriver.exe";
     private static final Logger LOGGER = LogManager.getLogger(Core.class);
-
     int i = 0;
-    //private List<WebElement> outputList;
-    //List<WebElement> outputList;
+
 
     public Core() {
         System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_LOCATION);
@@ -24,37 +22,24 @@ public class Core {
     }
 
     public void openWebPage(String url) {
-        if (!url.contains("http://") && !url.contains("https://")) { url = "http://" + url; }
+        if (!url.contains("http://") && !url.contains("https://")) {
+            url = "http://" + url;
+        }
         LOGGER.info("Opening web page: " + url);
         driver.get(url);
     }
-    public void closeDriver(){
+
+    public void closeDriver() {
         driver.close();
     }
 
-    public List<WebElement> getListOfElements (By path){
+    public List<WebElement> getListOfElements(By path) {
         List<WebElement> elements = driver.findElements(path);
         return elements;
     }
 
-    /*public List<WebElement> selectFromListOfElements (List<WebElement> inputList, String criteria){
-        //int i = 0;
 
-        for (WebElement ele : inputList){
-            if (ele.getText().contains(criteria)){
-                List<WebElement> outputList[i] = ele;
-                i = i + 1;
-            }
-        }
-    }*/
-
-    public void printTablename(By path){
-        //WebDriverWait wait = new WebDriverWait(driver, 5);
-        //wait.until(ExpectedConditions.visibilityOfElementLocated(path));
-        System.out.println(driver.findElement(path).getText());
-    }
-
-    public void waitPageLoaded(){
+    public void waitPageLoaded() {
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 }
