cbaf5e83f4aa49612d5f86bbcf8c3c9ec1d8549e
Romans Dobrijans <rdobrijans@evolutiongaming.com>
2018-08-08 10:29:30 +0300

    Finalised version on 06.08.2018

--- src/test/java/Launcher.java
+++ src/test/java/Launcher.java
@@ -14,12 +14,16 @@ public class Launcher {
     private static final Logger LOGGER = LogManager.getLogger(Launcher.class);
     private List<WebElement> tableNames;
     private List<WebElement> tables;
+    private List<WebElement> tableStatus;
+    private List<WebElement> tableStatusNames;
     int i = 0;
 
     private static final String NAME = "Tex_"; //Change to necessary table name component
 
     private static final By TABLE_NAMES = By.xpath("//tr/td[4][contains(text()[2],'"+NAME+"')]/../td[8][contains(b, 'Finished') or contains(b, 'Failed')]/../td[10]/../td[4]");
     private static final By TABLES_LIST = By.xpath("//tr/td[4][contains(text()[2],'"+NAME+"')]/../td[8][contains(b, 'Finished') or contains(b, 'Failed')]/../td[10]");
+    private static final By TABLES_STATUS = By.xpath("//tr/td[4][contains(text()[2],'"+NAME+"')]/../td[8]");
+    private static final By TABLES_STATUS_NAME = By.xpath("//tr/td[4][contains(text()[2],'"+NAME+"')]/../td[8]/../td[4]");
 
 
     private void openHomePageDesk() {
@@ -34,10 +38,21 @@ public class Launcher {
 
         tableNames = core.getListOfElements(TABLE_NAMES);
         tables = core.getListOfElements(TABLES_LIST);
+        tableStatus = core.getListOfElements(TABLES_STATUS);
+        tableStatusNames = core.getListOfElements(TABLES_STATUS_NAME);
+
+        System.out.println("Table status:");
+
+        for (i = 0; i < tableStatus.size(); i++) {
+            System.out.println(i + "  " + tableStatusNames.get(i).getText() + "  " + tableStatus.get(i).getText());
+        }
+
         Assert.assertFalse("No tables with Finished or Failed status", tables.isEmpty());
 
+        System.out.println("Starting tables with 'Finished' or 'Failed' status:");
+
         for (i = 0; i < tables.size(); i++) {
-            System.out.println("Clicking table Nr." + i + "  " + tableNames.get(i).getText());
+            System.out.println(i + "Clicking table: " + tableNames.get(i).getText());
             tables.get(i).click();
         }
     }
