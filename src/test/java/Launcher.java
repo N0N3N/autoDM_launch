71936ddc27f17cd45522fb428673cf83626b9aad
Romans Dobrijans <rdobrijans@evolutiongaming.com>
2018-08-08 13:36:15 +0300

    Finalised version on 08.08.2018 version is adapted to fast empty check and check for tables in semi-state

--- src/test/java/Launcher.java
+++ src/test/java/Launcher.java
@@ -9,8 +9,6 @@ import java.util.List;
 
 public class Launcher {
     private Core core = new Core();
-    private static final String WEB_PAGE = "http://autodm:8080/assets/index.html#/dealers";
-
     private static final Logger LOGGER = LogManager.getLogger(Launcher.class);
 
     private int i;
@@ -20,11 +18,12 @@ public class Launcher {
     private static final String NAME = "Tex_"; //Change to necessary table name component
     //private static final String NAME = "DNT release QA";
 
+    private static final String WEB_PAGE = "http://autodm:8080/assets/index.html#/dealers";
     private static final String FINISHED = "Finished";
     private static final String FAILED = "Failed";
     private static final String RUNNING = "Running";
-    private static final By TABLE_NAMES = By.xpath("//tr/td[4][contains(text()[2],'" + NAME + "')]/../td[8][contains(b, '"+FINISHED+"') or contains(b, '"+FAILED+"')]/../td[10]/../td[4]");
-    private static final By TABLES_LIST = By.xpath("//tr/td[4][contains(text()[2],'" + NAME + "')]/../td[8][contains(b, '"+FINISHED+"') or contains(b, '"+FAILED+"')]/../td[10]");
+    private static final By TABLE_NAMES = By.xpath("//tr/td[4][contains(text()[2],'" + NAME + "')]/../td[8][contains(b, '" + FINISHED + "') or contains(b, '" + FAILED + "')]/../td[10]/../td[4]");
+    private static final By TABLES_LIST = By.xpath("//tr/td[4][contains(text()[2],'" + NAME + "')]/../td[8][contains(b, '" + FINISHED + "') or contains(b, '" + FAILED + "')]/../td[10]");
     private static final By TABLES_STATUS = By.xpath("//tr/td[4][contains(text()[2],'" + NAME + "')]/../td[8]");
     private static final By TABLES_STATUS_NAME = By.xpath("//tr/td[4][contains(text()[2],'" + NAME + "')]/../td[8]/../td[4]");
 
@@ -34,7 +33,6 @@ public class Launcher {
         core.openWebPage(WEB_PAGE);
         core.waitPageLoaded();
     }
-
 
     private void printTableStatus() {
         LOGGER.info("Collecting table status info");
@@ -48,29 +46,28 @@ public class Launcher {
             if (tableStatus.get(i).getText().contains(FINISHED) || tableStatus.get(i).getText().contains(FAILED)) {
                 numberToStart = numberToStart + 1;
             }
-            if (!tableStatus.get(i).getText().contains(FINISHED) && !tableStatus.get(i).getText().contains(FAILED)&& !tableStatus.get(i).getText().contains(RUNNING)) {
+            if (!tableStatus.get(i).getText().contains(FINISHED) && !tableStatus.get(i).getText().contains(FAILED) && !tableStatus.get(i).getText().contains(RUNNING)) {
                 numberSemiState = numberSemiState + 1;
             }
         }
         System.out.println("  ");
         System.out.println("Number of tables to start: " + numberToStart);
-        if (numberSemiState < 0) {
+        if (numberSemiState > 0) {
             System.out.println("  ");
-            System.out.println("WARNING: Number of tables with semi-state: " +numberSemiState+" !     Recommended to wait and relaunch tables later");
+            System.out.println("WARNING: Number of tables with semi-state: " + numberSemiState + " !     Recommended to wait and relaunch tables later");
         }
         if (numberToStart == 0) {
-            System.out.println("No tables with "+FINISHED+" or "+FAILED+" status");
+            System.out.println("No tables with " + FINISHED + " or " + FAILED + " status");
             core.closeDriver();
             System.exit(100);
         }
     }
 
-
     private void startTables() {
         List<WebElement> tableNames = core.getListOfElements(TABLE_NAMES);
         List<WebElement> tables = core.getListOfElements(TABLES_LIST);
 
-        LOGGER.info("Starting tables with "+FINISHED+" or "+FAILED+" status:");
+        LOGGER.info("Starting tables with " + FINISHED + " or " + FAILED + " status:");
         System.out.println("  ");
         System.out.println("Starting tables:");
         for (i = 0; i < tables.size(); i++) {
